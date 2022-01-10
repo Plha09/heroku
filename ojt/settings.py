77e47eb66ebcba6d926af6a12a494d3b122de023
Plha09 <97078210+Plha09@users.noreply.github.com>
2022-01-10 14:50:51 +0600

    Update settings.py

--- ojt/settings.py
+++ ojt/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+znstwa&n%07a-^d361@g)sob9kf84yyx5oh=okxhltr7(%1to'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'plha.herokuapp.com']
 
